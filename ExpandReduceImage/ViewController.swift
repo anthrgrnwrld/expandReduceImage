52849317944263aedeea6779240e638afb29df75
anthrgrnwrld <anthrgrnwrld@gmail.com>
2016-02-02 21:09:50 +0900

    Initial Commit

---
+++ ExpandReduceImage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ExpandReduceImage
+//
+//  Created by Masaki Horimoto on 2016/02/02.
+//  Copyright © 2016年 Masaki Horimoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
